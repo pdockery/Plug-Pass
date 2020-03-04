d6664e4c397b60917c0bbd428887b845c61d1184
Paul Dockery <61437469+pdockery@users.noreply.github.com>
2020-03-03 16:30:12 -0800

    commented out while loop
    there was a while look that kept looping and locked up the code.  commented out. also added some time prints to check if it went through the loop.

--- plug_pass_prototype_0_1.ino
+++ plug_pass_prototype_0_1.ino
@@ -180,12 +180,61 @@ void loop () {
     // Wait a bit before trying again
     Serial.println("\n\nSend a character to scan another tag!");
     Serial.flush();
-    while (!Serial.available());
+   // while (!Serial.available());
     while (Serial.available()) {
     Serial.read();
     }
     Serial.flush();    
   }
-  Serial.println();
+  else 
+    {
+      Serial.println("ahhhhhhhhhhhhh");
+    }
+    DateTime now = rtc.now();
+
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(" (");
+    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+
+    Serial.print(" since midnight 1/1/1970 = ");
+    Serial.print(now.unixtime());
+    Serial.print("s = ");
+    Serial.print(now.unixtime() / 86400L);
+    Serial.println("d");
+
+    // calculate a date which is 7 days and 30 seconds into the future
+    DateTime future (now + TimeSpan(7,12,30,6));
+
+    Serial.print(" now + 7d + 30s: ");
+    Serial.print(future.year(), DEC);
+    Serial.print('/');
+    Serial.print(future.month(), DEC);
+    Serial.print('/');
+    Serial.print(future.day(), DEC);
+    Serial.print(' ');
+    Serial.print(future.hour(), DEC);
+    Serial.print(':');
+    Serial.print(future.minute(), DEC);
+    Serial.print(':');
+    Serial.print(future.second(), DEC);
+    Serial.println();
+
+    Serial.print("Temperature: ");
+    Serial.print(rtc.getTemperature());
+    Serial.println(" C");
+
+    Serial.println();
+    
   delay(3000);
 }
